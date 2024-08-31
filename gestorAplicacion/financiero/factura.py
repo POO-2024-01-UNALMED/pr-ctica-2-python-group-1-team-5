e98aa0f7721d2fc8d38c918cefc2f3a876c1198d
SebasGuerrag1 <159485822+SebasGuerrag1@users.noreply.github.com>
2024-08-31 13:09:21 -0500

    .

--- gestorAplicacion/financiero/factura.py
+++ gestorAplicacion/financiero/factura.py
@@ -1,2 +1,131 @@
+from datetime import datetime
+from typing import List
+
 class Factura():
-    pass
+    # Atributos de clase
+    IVA = 0.19
+    facturas_creadas = 0
+    facturas = []
+
+    def __init__(self, producto=None, precio=0, fecha=None, cliente=None, entidad=None, servicio="Inventario", lista_productos=None):
+        # Atributos de instancia
+        self.ID = Factura.facturas_creadas + 1
+        Factura.facturas_creadas = self.ID
+        self.producto = producto
+        self.precio = precio
+        self.total = 0
+        self.cliente = cliente
+        self.entidad = entidad
+        self.fecha = fecha 
+        self.precio_final = 0
+        self.lista_productos = lista_productos if lista_productos else []
+        self.porcentaje_credito_por_pagar = 1.0
+        self.servicio = servicio
+        
+        if not lista_productos:
+            self.calcular_total()
+        else:
+            self.ajustar_productos()
+            self.total_factura()
+
+        Factura.facturas.append(self)
+    #calcula el total del precio de la factura
+    def total_factura(self):
+        self.total = sum(producto.precio * producto.cantidad for producto in self.lista_productos)
+        return self.total
+    
+    def aplicar_descuento(self, porcentaje):
+        if 0 < porcentaje <= 100:
+            self.precio -= self.precio * (porcentaje / 100)
+    #aplica el iva al precio total
+    def calcular_total(self):
+        self.total = self.precio + (self.precio * self.IVA)
+
+    
+    # Getters para atributos de clase
+    @classmethod
+    def getIVA(cls):
+        return cls.IVA
+
+    @classmethod
+    def getFacturasCreadas(cls):
+        return cls.facturas_creadas
+
+    @classmethod
+    def getFacturas(cls):
+        return cls.facturas
+
+    # Setters para atributos de clase
+    @classmethod
+    def setIVA(cls, IVA):
+        cls.IVA = IVA
+
+    @classmethod
+    def setFacturasCreadas(cls, facturas_creadas):
+        cls.facturas_creadas = facturas_creadas
+
+    @classmethod
+    def setFacturas(cls, facturas):
+        cls.facturas = facturas
+    
+    # Getters
+    def getProducto(self):
+        return self.producto
+
+    def getPrecio(self):
+        return self.precio
+
+    def getTotal(self):
+        return self.total
+
+    def getCliente(self):
+        return self.cliente
+
+    def getEntidad(self):
+        return self.entidad
+
+    def getFecha(self):
+        return self.fecha
+
+    def getPrecioFinal(self):
+        return self.precio_final
+
+    def getListaProductos(self):
+        return self.lista_productos
+
+    def getPorcentajeCreditoPorPagar(self):
+        return self.porcentaje_credito_por_pagar
+
+    def getServicio(self):
+        return self.servicio
+
+    # Setters
+    def setProducto(self, producto):
+        self.producto = producto
+
+    def setPrecio(self, precio):
+        self.precio = precio
+
+    def setTotal(self, total):
+        self.total = total
+
+    def setCliente(self, cliente):
+        self.cliente = cliente
+
+    def setEntidad(self, entidad):
+        self.entidad = entidad
+
+    def setFecha(self, fecha):
+        self.fecha = fecha
+
+    def setPrecioFinal(self, precio_final):
+        self.precio_final = precio_final
+
+    def setListaProductos(self, lista_productos):
+        self.lista_productos = lista_productos
+
+    def setPorcentajeCreditoPorPagar(self, porcentaje_credito_por_pagar):
+        self.porcentaje_credito_por_pagar = porcentaje_credito_por_pagar
+
+    def setServicio(self, servicio):
+        self.servicio = servicio
--- gestorAplicacion/inventario/tipoVehiculo.py
+++ gestorAplicacion/inventario/tipoVehiculo.py
@@ -1,2 +1,50 @@
+from enum import Enum
+
 class TipoVehiculo():
-    pass
+    BERLINA = (4, False, True, 70000)
+    CARROZA = (6, True, False, 150000)
+    FAETON = (4, False, True, 120000)
+    COCHEFUNEBRE = (1, True, False, 80000)
+    BUS = (6, False, True, 50000)
+    COCHERESPETO = (8, False, True, 75000)
+    CUPE = (4, False, True, 65000)
+    CAMION = (5, False, False, 69000)
+
+    def __init__(self, capacidad, cliente, familiar, precio):
+        self._capacidad = capacidad
+        self._cliente = cliente
+        self._familiar = familiar
+        self._precio = precio
+
+    def __str__(self):
+        return f"Vehiculo tipo {self.name} - Capacidad: {self.capacidad}"
+    
+    #getters 
+    
+    def getCapacidad(self):
+        return self._capacidad
+    
+    def getCliente(self):
+        return self._cliente
+    
+    def getFamiliar(self):
+        return self._familiar
+    
+    def getPrecio(self):
+        return self._precio
+    
+    #setters
+
+    def setCapacidad(self, capacidad):
+        self._capacidad = capacidad
+    
+    def setCliente(self, cliente):
+        self._cliente = cliente
+    
+    def setFamiliar(self,familiar):
+        self._familiar = familiar
+    
+    def setPrecio(self,precio):
+        self._precio = precio
+
+
