25df9f4c8e6d7fa8f2890d7baeb265067b640682
violeta456 <159485730+violeta456@users.noreply.github.com>
2024-09-03 07:25:54 -0500

    enumIglesia

--- gestorAplicacion/establecimientos/iglesia.py
+++ gestorAplicacion/establecimientos/iglesia.py
@@ -3,76 +3,84 @@ from typing import List, Optional
 from datetime import datetime, time, timedelta
 
 class Iglesia(Enum):
-    Hinduismo = ("Kamakhya Temple", 7, True, 4, ["ordinaria"], "Pundit", "Guru")
-    Budismo = ("Templo de Nanjing", 8, True, 3, ["fija", "ordinaria"], "Monje budista", "Abad")
-    Cristianismo = ("Iglesia de la Sagrada Familia", 5, True, 1, ["fija", "ordinaria"], "Padre", "Obispo")
-    Islam = ("Mezquita del Profeta", 6, False, 0, None, "Imán", "Sheij")
-    Judaísmo = ("Sinagoga de la Rua", 8, False, 0, None, "Rabino", "Jazan")
-    Taoísmo = ("Templo del Valle del Jade", 3, True, 2, ["fija"], "Daoist", "Dao Shi")
+    
+    HINDUISMO = {
+        "nombre": "Kamakhya Temple",
+        "sillas": 7,
+        "cremacion": True,
+        "duracionEvento": 4,
+        "tipoUrna": ["ordinaria"],
+        "religioso": "Pundit",
+        "religiosoAltoRango": "Guru"
+    }
 
-    def __init__(self, nombre: str, sillas: int, cremacion: bool, duracionEvento: int, tipoUrnas: Optional[List[str]], religioso: str, religiosoAltoRango: str):
-        self._nombre = nombre
-        self._sillas = sillas
-        self._cremacion = cremacion
-        self._duracionEvento = duracionEvento
-        self._tipoUrnas = tipoUrnas if tipoUrnas is not None else []
-        self._religioso = religioso
-        self._religiosoAltoRango = religiosoAltoRango
+    BUDISMO = {
+        "nombre": "Templo de Nanjing",
+        "sillas": 8,
+        "cremacion": True,
+        "duracionEvento": 3,
+        "tipoUrna": ["fija", "ordinaria"],
+        "religioso": "Monje budista",
+        "religiosoAltoRango": "Abad"
+    }
 
-    @property
-    def nombre(self):
-        return self._nombre
+    CRISTIANISMO = {
+        "nombre": "Iglesia de la Sagrada Familia",
+        "sillas": 5,
+        "cremacion": True,
+        "duracionEvento": 1,
+        "tipoUrna": ["fija", "ordinaria"],
+        "religioso": "Padre",
+        "religiosoAltoRango": "Obispo"
+    }
 
-    @property
-    def sillas(self):
-        return self._sillas
+    ISLAM = {
+        "nombre": "Mezquita del Profeta",
+        "sillas": 6,
+        "cremacion": False,
+        "duracionEvento": 0,
+        "tipoUrna": None,
+        "religioso": "Imán",
+        "religiosoAltoRango": "Sheij"
+    }
 
-    @property
-    def cremacion(self):
-        return self._cremacion
+    JUDAISMO = {
+        "nombre": "Sinagoga de la Rua",
+        "sillas": 8,
+        "cremacion": False,
+        "duracionEvento": 0,
+        "tipoUrna": None,
+        "religioso": "Rabino",
+        "religiosoAltoRango": "Jazan"
+    }
 
-    @property
-    def duracionEvento(self):
-        return self._duracionEvento
+    TAOISMO = {
+        "nombre": "Templo del Valle del Jade",
+        "sillas": 3,
+        "cremacion": True,
+        "duracionEvento": 2,
+        "tipoUrna": ["fija"],
+        "religioso": "Daoist",
+        "religiosoAltoRango": "Dao Shi"
+    }
+    
+    def getNombre(self):
+        return self.value.get("nombre")
 
-    @property
-    def tipoUrnas(self):
-        return self._tipoUrnas
+    def getSillas(self):
+        return self.value.get("sillas")
 
-    @property
-    def religioso(self):
-        return self._religioso
+    def getCremacion(self):
+        return self.value.get("cremacion")
 
-    @property
-    def religiosoAltoRango(self):
-        return self._religiosoAltoRango
+    def getDuracion_evento(self):
+        return self.value.get("duracionEvento")
 
-    def getDuracionCremacion(self):
-        return self.duracionEvento
+    def getTipoUrna(self):
+        return self.value.get("tipoUrna")
 
-    def duracionEventoEnd(self, horaInicio: time) -> time:
-        """Calcula la hora de finalización del evento a partir de la hora de inicio."""
-        horaInicioDt = datetime.combine(datetime.today(), horaInicio)
-        horaFinEventoDt = horaInicioDt + timedelta(hours=self.duracionEvento)
-        return horaFinEventoDt.time()
+    def getReligioso(self):
+        return self.value.get("religioso")
 
-    def setDuracionEvento(self, duracionEvento: int):
-        self._duracionEvento = duracionEvento
-
-    def setNombre(self, nombre: str):
-        self._nombre = nombre
-
-    def setSillas(self, sillas: int):
-        self._sillas = sillas
-
-    def setCremacion(self, cremacion: bool):
-        self._cremacion = cremacion
-
-    def setTipoUrnas(self, tipoUrnas: Optional[List[str]]):
-        self._tipoUrnas = tipoUrnas if tipoUrnas is not None else []
-
-    def setReligioso(self, religioso: str):
-        self._religioso = religioso
-
-    def setReligiosoAltoRango(self, religiosoAltoRango: str):
-        self._religiosoAltoRango = religiosoAltoRango
+    def getReligiosoAltoRango(self):
+        return self.value.get("religiosoAltoRango")
